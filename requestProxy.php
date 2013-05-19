9a86bc171851e7fa200e79ae3e8faddaabed08d8
Brian Fagan <fagan@gigya.com>
2013-05-19 16:25:31 -0700

    -Routed the requests through a PHP proxy to resolve issues with saving a large schema. Fixes #4. Fixes #2. -Updated/fixed the "Remove" feature to now send fields a "null" to setSchema. -Added html escaping for the regex field which can contain quotes that broke the value rendering. Fixes #1. Fixes #5. -Removed the dynamicFields parameter since it is now deprecated by Gigya. All getSchema calls default to "filter: explicitOnly"

---
+++ requestProxy.php
@@ -0,0 +1,44 @@
+<?
+// Some basic protection against anyone using the proxy (I know the referer can be spoofed)
+if (preg_match("/".preg_quote('brian-fagan.com',"/")."/i",$_SERVER['HTTP_REFERER']) === FALSE) {
+   die('Invalid referrer.');
+}
+
+// Use Gigya's SSL cert for HTTPS requests
+$cafile = realpath('../../cacert.pem');
+
+// Construct the Gigya endpoint
+$tokens =  explode(".",$_POST['apiMethod']);
+$namespace = $tokens[0];
+$apiURL = 'https://' . $namespace . '.' . $_POST['dataCenter'] . '.gigya.com/' . $_POST['apiMethod'];
+
+// Remove the values from what will be sent to Gigya
+unset($_POST['dataCenter']);
+unset($_POST['apiMethod']);
+
+// Check to see if libcurl is installed			
+if (!function_exists('curl_init')){
+	die("Sending this request requires PHP to be compiled with libcurl.");
+}
+
+// Now send the request using the data that was posted to us
+$ch = curl_init();
+$defaults = array(
+			CURLOPT_URL => $apiURL,
+			CURLOPT_POST=>1,
+			CURLOPT_HEADER => 1,
+			CURLOPT_POSTFIELDS=> $_POST,
+			CURLOPT_HTTPHEADER => array( 'Expect:'),
+			CURLOPT_RETURNTRANSFER => TRUE,
+			CURLOPT_SSL_VERIFYPEER => TRUE, 
+			CURLOPT_SSL_VERIFYHOST => 2,
+			CURLOPT_CAINFO => $cafile,
+			CURLOPT_HEADER => false,
+		);
+		
+$ch = curl_init();
+curl_setopt_array($ch, $defaults);
+$response = curl_exec($ch);
+curl_close($ch);
+echo $response;
+?>

81346316ae631a7eea4aa6687c67032667453c69
Brian Fagan <fagan@gigya.com>
2015-09-30 16:57:28 -0400

    Overhaul of schema editor: -Removed the concept of a viewer and editor. Everything is editable now. -Added support for managing a Data Store schema

--- requestProxy.php
+++ requestProxy.php
@@ -1,27 +1,27 @@
-<?
-// Some basic protection against anyone using the proxy (I know the referer can be spoofed)
-if (preg_match("/".preg_quote('brian-fagan.com',"/")."/i",$_SERVER['HTTP_REFERER']) === FALSE) {
+<?php
+//Some basic protection against anyone using the proxy (I know the referer can be spoofed)
+if (preg_match("/".preg_quote('gigya-cs.com',"/")."/i",$_SERVER['HTTP_REFERER']) === FALSE) {
    die('Invalid referrer.');
 }
 
-// Use Gigya's SSL cert for HTTPS requests
+//Use Gigya's SSL cert for HTTPS requests
 $cafile = realpath('../../cacert.pem');
 
-// Construct the Gigya endpoint
+//Construct the Gigya endpoint
 $tokens =  explode(".",$_POST['apiMethod']);
 $namespace = $tokens[0];
 $apiURL = 'https://' . $namespace . '.' . $_POST['dataCenter'] . '.gigya.com/' . $_POST['apiMethod'];
 
-// Remove the values from what will be sent to Gigya
+//Remove the values from what will be sent to Gigya
 unset($_POST['dataCenter']);
 unset($_POST['apiMethod']);
 
-// Check to see if libcurl is installed			
+//Check to see if libcurl is installed			
 if (!function_exists('curl_init')){
 	die("Sending this request requires PHP to be compiled with libcurl.");
 }
 
-// Now send the request using the data that was posted to us
+//Now send the request using the data that was posted to us
 $ch = curl_init();
 $defaults = array(
 			CURLOPT_URL => $apiURL,
